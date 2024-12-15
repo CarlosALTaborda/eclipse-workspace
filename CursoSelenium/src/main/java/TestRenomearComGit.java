7f2c010fa6e338330e4e08ead885a35d357fb6b2
CarlosALTaborda <calt.alexandre@gmail.com>
2024-12-15 17:39:24 -0300

    adicionando arquivo para teste de renomear

---
+++ CursoSelenium/src/main/java/TestRenomearComGit.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestRenomearComGit {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
